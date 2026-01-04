fae007c43200c2b2ee3f477e6ba72031caa6388e
Fugi <nikoplush11@gmail.com>
2026-01-04 16:25:32 +0000

    Add files via upload

--- mango/autostart.sh
+++ mango/autostart.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 waybar -c ~/.config/mango/config.jsonc -s ~/.config/mango/style.css >/dev/null 2>&1 &
-swaybg -i /path/to/wallpaper >/dev/null 2>&1 &
+swaybg -i /mnt/Ddrive/Wallpapers/WallpaperFixeN64.png >/dev/null 2>&1 &
 wl-clip-persist --clipboard regular --reconnect-tries 0 >/dev/null 2>&1 &
 wl-paste --type text --watch cliphist store >/dev/null 2>&1 &
 dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=MangoWC

5d3c9527bd00afa4dffb0f5eababc93446187da3
Fugi <nikoplush11@gmail.com>
2026-01-04 00:25:12 +0000

    Add files via upload

---
+++ mango/autostart.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+waybar -c ~/.config/mango/config.jsonc -s ~/.config/mango/style.css >/dev/null 2>&1 &
+swaybg -i /path/to/wallpaper >/dev/null 2>&1 &
+wl-clip-persist --clipboard regular --reconnect-tries 0 >/dev/null 2>&1 &
+wl-paste --type text --watch cliphist store >/dev/null 2>&1 &
+dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=MangoWC
+systemctl --user restart xdg-desktop-portal xdg-desktop-portal-wlr
+echo "Xft.dpi: 140" | xrdb -merge #dpi缩放
